9eb5eef28edc29a74c8730d6f790d2d83ae831d8
simonrosengren <simonrosengren123@gmail.com>
2018-02-27 10:21:49 +0100

    Adding .ignore and GoogleApiHandler

---
+++ Source/Loader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ColossalFramework;
+using ICities;
+
+namespace CitiesConext.Source
+{
+    public class Loader : LoadingExtensionBase
+    {
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            GoogleApiHandler googleApiHandler = new GoogleApiHandler();
+            googleApiHandler.SendRefreshTokenRequestRequest();
+        }
+    }
+}
--- Source/Mod.cs
+++ Source/Mod.cs
@@ -22,7 +22,7 @@ namespace CitiesConext
         {
             get
             {
-                return "MHMHHHMH HERROW Worrd";
+                return "CitiesContext";
             }
         }
     }
