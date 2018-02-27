7d1e4086d9b9194e541ca1d0e2c658960dea8919
LuddeW <ludwig.wiktorsson@gmail.com>
2018-02-27 10:04:01 +0100

    init

---
+++ Source/Mod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICities;
+
+namespace CitiesConext
+{
+    public class Mod : IUserMod
+    {
+        public string Description
+        {
+            get
+            {
+                return "This is a mod";
+            }
+
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "MHMHHHMH HERROW Worrd";
+            }
+        }
+    }
+}
