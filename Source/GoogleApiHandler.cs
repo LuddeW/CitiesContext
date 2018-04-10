5be2df99fd4a35fe4d4a12c8d6b3a51543c733e7
simonrosengren <simonrosengren123@gmail.com>
2018-04-10 15:16:47 +0200

    Removing hardcoded call to tax multiplier

--- Source/GoogleApiHandler.cs
+++ Source/GoogleApiHandler.cs
@@ -15,9 +15,9 @@ namespace CitiesConext
         string postData;
         string accessToken = "";
         List<SpeedModel> speedModels = new List<SpeedModel>();
-        private static string refresh_token = "1/hGTNfC-LKRsYk8ms0xrZeJsj1TsXfr11m9UqDx285gw";
-        private static string client_id = "201588886496-h7an999j4h8sdir55q697m3svsta00ei.apps.googleusercontent.com";
-        private static string client_secret = "PuaqSlu3cyS6KAm7E9iaMBYN";
+        private static string refresh_token = "1/fsklTpeWGTdYt4QfDXFRexlqkIlSE2vddDxPdzR3-7s";
+        private static string client_id = "643711194524-fvtr98u75reat0jh3e3re2dh2vnpjimh.apps.googleusercontent.com";
+        private static string client_secret = "IZd9zOGgYkq9Zkdu4EE0W9RT";
 
         long now;
         long then;
